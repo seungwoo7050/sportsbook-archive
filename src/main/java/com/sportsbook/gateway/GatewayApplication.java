6bf7a9ff519a3f91a13d8a68a6adbf101b8c0142
seungwoo7050 <seungwoo7050@naver.com>
2026-08-21 20:59:33 +0900

    feat(application): start API gateway

---
+++ src/main/java/com/sportsbook/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.sportsbook.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+}
