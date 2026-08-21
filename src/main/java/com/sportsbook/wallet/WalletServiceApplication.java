8ebceef58ddd65f757dd0d92bc1d4c4322ab66d3
seungwoo7050 <seungwoo7050@naver.com>
2026-08-21 15:47:43 +0900

    config(outbox): activate safe delivery polling

--- src/main/java/com/sportsbook/wallet/WalletServiceApplication.java
+++ src/main/java/com/sportsbook/wallet/WalletServiceApplication.java
@@ -2,8 +2,10 @@ package com.sportsbook.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class WalletServiceApplication {
 
