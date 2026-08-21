7c698036d84c20a9144b8d2b5e2e4424942d922b
seungwoo7050 <seungwoo7050@naver.com>
2026-08-21 15:29:28 +0900

    build(security): add internal request security

--- src/main/java/com/sportsbook/risk/RiskServiceApplication.java
+++ src/main/java/com/sportsbook/risk/RiskServiceApplication.java
@@ -2,9 +2,10 @@ package com.sportsbook.risk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @ConfigurationPropertiesScan
 @SuppressWarnings("HideUtilityClassConstructor")
 public class RiskServiceApplication {
