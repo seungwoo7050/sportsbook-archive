06a88fdb449f2e155950605fea722c54d425dc50
seungwoo7050 <seungwoo7050@naver.com>
2026-08-22 20:09:13 +0900

    test(config): isolate background workers

--- src/test/java/com/sportsbook/settlement/SettlementServiceApplicationTest.java
+++ src/test/java/com/sportsbook/settlement/SettlementServiceApplicationTest.java
@@ -3,10 +3,12 @@ package com.sportsbook.settlement;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sportsbook.settlement.config.RawKafkaProducerConfiguration;
+import com.sportsbook.settlement.config.SettlementWorkerConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.core.KafkaTemplate;
 
@@ -18,9 +20,11 @@ class SettlementServiceApplicationTest {
   private KafkaOperations<byte[], byte[]> rawOperations;
 
   @Autowired private KafkaTemplate<Object, Object> defaultTemplate;
+  @Autowired private ApplicationContext context;
 
   @Test
   void loadsApplicationContextWithIsolatedKafkaTemplates() {
     assertThat(rawOperations).isNotSameAs(defaultTemplate);
+    assertThat(context.containsBean(SettlementWorkerConfiguration.RECOVERY)).isFalse();
   }
 }
