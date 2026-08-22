30adb4d75b5201ca628e6a5d479ea65c17f2b8df
seungwoo7050 <seungwoo7050@naver.com>
2026-08-22 21:11:18 +0900

    test(integration): isolate PostgreSQL contexts

--- src/test/java/com/sportsbook/betting/support/PostgresIntegrationSupport.java
+++ src/test/java/com/sportsbook/betting/support/PostgresIntegrationSupport.java
@@ -1,5 +1,6 @@
 package com.sportsbook.betting.support;
 
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -7,6 +8,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class PostgresIntegrationSupport {
 
   @Container
