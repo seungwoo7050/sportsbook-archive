b210312dcd50359c1001c55db269e012aea62547
seungwoo7050 <seungwoo7050@naver.com>
2026-08-22 10:00:40 +0900

    test(kafka): load isolated producer factories

--- src/test/java/com/sportsbook/settlement/SettlementServiceApplicationTest.java
+++ src/test/java/com/sportsbook/settlement/SettlementServiceApplicationTest.java
@@ -1,11 +1,26 @@
 package com.sportsbook.settlement;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.sportsbook.settlement.config.RawKafkaProducerConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class SettlementServiceApplicationTest {
 
+  @Autowired
+  @Qualifier(RawKafkaProducerConfiguration.OPERATIONS)
+  private KafkaOperations<byte[], byte[]> rawOperations;
+
+  @Autowired private KafkaTemplate<Object, Object> defaultTemplate;
+
   @Test
-  void loadsApplicationContext() {}
+  void loadsApplicationContextWithIsolatedKafkaTemplates() {
+    assertThat(rawOperations).isNotSameAs(defaultTemplate);
+  }
 }
