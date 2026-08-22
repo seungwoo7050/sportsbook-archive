3fc8f3710685299c36d40f6def0eb9f8b219e360
seungwoo7050 <seungwoo7050@naver.com>
2026-08-22 09:24:27 +0900

    test(application): load minimal context

---
+++ src/test/java/com/sportsbook/settlement/SettlementServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.sportsbook.settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementServiceApplicationTest {
+
+  @Test
+  void loadsApplicationContext() {}
+}
