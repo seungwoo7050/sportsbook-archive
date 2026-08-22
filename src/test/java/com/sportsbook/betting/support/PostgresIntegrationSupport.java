2de78ea64d1d91062a8bb33c3765d36f6423b1ce
seungwoo7050 <seungwoo7050@naver.com>
2026-08-22 11:34:26 +0900

    test(integration): provide PostgreSQL runtime

---
+++ src/test/java/com/sportsbook/betting/support/PostgresIntegrationSupport.java
@@ -0,0 +1,25 @@
+package com.sportsbook.betting.support;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class PostgresIntegrationSupport {
+
+  @Container
+  protected static final PostgreSQLContainer<?> POSTGRES =
+      new PostgreSQLContainer<>("postgres:16-alpine")
+          .withDatabaseName("betting")
+          .withUsername("betting")
+          .withPassword("betting");
+
+  @DynamicPropertySource
+  static void postgresProperties(DynamicPropertyRegistry properties) {
+    properties.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+    properties.add("spring.datasource.username", POSTGRES::getUsername);
+    properties.add("spring.datasource.password", POSTGRES::getPassword);
+  }
+}
