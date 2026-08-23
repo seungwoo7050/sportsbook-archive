7ef35bbed1a5ff909a66fc4d383c3c195807ed27
seungwoo7050 <seungwoo7050@naver.com>
2026-08-23 15:12:22 +0900

    test(build): require executable settlement packaging

--- src/test/java/com/sportsbook/settlement/BuildBaselineTest.java
+++ src/test/java/com/sportsbook/settlement/BuildBaselineTest.java
@@ -3,6 +3,9 @@ package com.sportsbook.settlement;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sportsbook.protocol.value.Money;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 
 class BuildBaselineTest {
@@ -11,5 +14,11 @@ class BuildBaselineTest {
   void targetsJava17AndLinksSharedProtocol() {
     assertThat(Runtime.version().feature()).isGreaterThanOrEqualTo(17);
     assertThat(Money.class.getPackageName()).isEqualTo("com.sportsbook.protocol.value");
+  }
+
+  @Test
+  void packagesTheServiceAsAnExecutableBootJar() throws IOException {
+    assertThat(Files.readString(Path.of("pom.xml")))
+        .contains("<artifactId>spring-boot-maven-plugin</artifactId>");
   }
 }
